52de8bcaf7846bd61a8a7220d32558491cd90039
haobingo <15879624465@sina.cn>
2024-09-26 22:04:56 +0800

    2024.9.26 路线选择页面设计

---
+++ RuoYi-App/components/sa-empty/sa-empty.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="empty">
+		<image class="emptyImg" src="@/static/empty.png" mode="widthFix"></image>
+		<view class="emptyText">{{msg}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			msg: {
+				type: String,
+				default: "暂无数据"
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.empty {
+		width: 100%;
+
+		.emptyImg {
+			display: block;
+			width: 160px;
+			margin: 60px auto 0;
+		}
+
+		.emptyText {
+			font-size: 14px;
+			font-weight: 400;
+			color: #333;
+			line-height: 1.5;
+			text-align: center;
+			padding: 10px 0;
+		}
+	}
+</style>
---
+++ RuoYi-App/pages/questionnaire/route_choice.vue
@@ -0,0 +1,127 @@
+<template>
+  <view>
+    <mlist :list="filteredList" @search="search"></mlist>
+  </view>
+</template>
+
+<script>
+  import Mlist from "@/components/m-list/m-list.vue";
+  export default {
+    components: {
+      Mlist,
+    },
+    data() {
+      return {
+        // 模拟的路线列表数据
+        list: [
+          {
+            name: "323",
+            length: 1.95,
+            elevation: 40,
+            image: "/static/images/1.jpg", // 使用实际图片路径
+            challenges: 1417,
+            remark: "风景优美，适合新手"
+          },
+          {
+            name: "世博依碧中心至金殿",
+            length: 1.95,
+            elevation: 40,
+            image: "/static/images/1.jpg", // 使用实际图片路径
+            challenges: 1417,
+            remark: "风景优美，适合新手"
+          },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/2.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  },
+		  {
+		    name: "世博依碧中心至金殿",
+		    length: 1.95,
+		    elevation: 40,
+		    image: "/static/images/1.jpg", // 使用实际图片路径
+		    challenges: 1417,
+		    remark: "风景优美，适合新手"
+		  }
+        ],
+        // 用于展示的过滤后列表
+        filteredList: [],
+      };
+    },
+    created() {
+      // 初始状态展示全部数据
+      this.filteredList = this.list;
+    },
+    methods: {
+      // 根据输入的关键字进行搜索
+      search({ keyword }) {
+        this.filteredList = this.list.filter((item) =>
+          item.name.includes(keyword)
+        );
+      },
+    },
+  };
+</script>
+
+<style>
+/* 样式保持不变或根据需要进行调整 */
+</style>
