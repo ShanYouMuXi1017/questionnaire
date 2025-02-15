e53474b80f5b9a8d6e6ae0ec0771d2321b697c8d
ShanYouMuXi <137975269+ShanYouMuXi1017@users.noreply.github.com>
2025-02-16 00:26:54 +0800

    0216版：二次解决网络连接异常问题

--- RuoYi-App/pages/index.vue
+++ RuoYi-App/pages/index.vue
@@ -1,135 +1,152 @@
 <template>
-  <view>
-    <view class="x-panel-top">
-      <view class="user-hint" style="text-align: center">
-        <text style="color: #555; font-size: 16px;font-weight:700">请选择你骑行过的路线进行打分吧！</text>
-      </view>
-    </view>
+	<view>
+		<view class="x-panel-top">
+			<view class="user-hint" style="text-align: center">
+				<text style="color: #555; font-size: 16px;font-weight:700">请选择你骑行过的路线进行打分吧！</text>
+			</view>
+		</view>
 
-    <view class="x-panel x-panel-content">
-      <view v-if="list.length > 0">
-        <!-- 遍历显示路线列表 -->
-        <view class="itemcss" v-for="(route, index) in list" :key="index">
-          <view :class="{'disabled-item': route.isAC === 1}" @click="handleClick(route)">
-            <view class="x-row">
-              <view class="x-col col-left">
-                <text class="tag">{{route.routerId}}</text>
-                <text>{{ route.routeName }}</text>
-              </view>
-            </view>
-            <view class="x-row" style="display: flex; align-items: center; position: relative;">
-              <view class="x-col col-left"
-                style="flex: 1; display: flex; align-items: center; text-align: left">
-                <!-- 图片 -->
-                <image style="width: 30%; height: 100px; margin-right: 10px" :src="route.imageUrl">
-                </image>
-                <!-- 全长 -->
-                <view style="margin-right: 10px">
-                  <text>全长: {{ route.length }} km</text>
-                </view>
-                <!-- 爬升高度 -->
-                <view>
-                  <text>爬升高度: {{ route.elevation }} m</text>
-                </view>
-              </view>
+		<view class="x-panel x-panel-content">
+			<view v-if="list.length > 0">
+				<!-- 遍历显示路线列表 -->
+				<view class="itemcss" v-for="(route, index) in list" :key="index">
+					<view :class="{'disabled-item': route.isAC === 1}" @click="handleClick(route)">
+						<view class="x-row" style="display: flex; align-items: center; position: relative;">
+							<view class="x-col col-left"
+								style="flex: 1; display: flex; align-items: center; text-align: left">
+								<!-- 图片 -->
+								<image style="width: 40%; height: 100px; margin-right: 10px" :src="route.imageUrl">
+								</image>
+								<!-- 全长 -->
+								<view class="x-row">
+									<view class="x-col col-left">
+										<text class="tag">{{index+1}}</text>
+										<text>{{ route.routeName }}</text>
+									</view>
+								</view>
 
-              <!-- 三角形水印 -->
-              <view v-if="route.isAC === 1" class="triangle-watermark">
-                <text class="triangle-text">已完成</text>
-              </view>
-            </view>
-          </view>
-          <view style="height: 10px"></view>
-        </view>
-      </view>
-      <view v-else>
-        <sa-empty></sa-empty>
-      </view>
-      <view v-if="hasMoreData">
-        <button class="load-more-button" @click="fetchRoutes" :disabled="isLoading" >加载更多</button>
-      </view>
-    </view>
-  </view>
+							</view>
+
+							<!-- 三角形水印 -->
+							<view v-if="route.isAC === 1" class="triangle-watermark">
+								<text class="triangle-text">已完成</text>
+							</view>
+						</view>
+					</view>
+					<view style="height: 10px"></view>
+				</view>
+			</view>
+			<view v-else>
+				<sa-empty></sa-empty>
+			</view>
+			<view v-if="hasMoreData">
+				<button class="load-more-button" @click="fetchRoutes" :disabled="isLoading">加载更多</button>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-import { toast } from "utils/common";
-import { getRouters } from "api/system/user";
+	import {
+		toast
+	} from "utils/common";
+	import {
+		getRouters
+	} from "api/system/user";
 
-export default {
-  name: "route-choice",
-  data() {
-    return {
-      list: [], // 原始路线列表
-	  isLoading: false, // 是否正在加载
-      queryParams: {
-        pageNum: 1,
-        pageSize: 3
-      },
-      hasMoreData: true // 是否还有更多数据
-    };
-  },
-  created() {
-    // 页面创建时加载数据
-    this.fetchRoutes();
-    wx.showShareMenu({
-      withShareTicket: true,
-      menus: ['shareAppMessage', 'shareTimeline']
-    });
-  },
-  methods: {
-    // 分享好友
-    onShareAppMessage() {
-      return {
-        title: '小枣营造社骑行小工具',
-        path: `pages/index`
-      }
-    },
-    // 分享朋友圈
-    onShareTimeline() {
-      return {
-        title: '小枣营造社骑行小工具',
-        path: `pages/index`
-      }
-    },
-    fetchRoutes() {
-		this.isLoading = true; // 设置为正在加载
-      getRouters(this.queryParams)
-        .then((res) => {
-          console.log(this.list);
-          if (res.data.length != 0) { // 请求的数据为空，没有数据
-            if (res.data.length == this.queryParams.pageSize) { // 请求的数据为4条，说明下一页可能还有数据，列表添加上新的数据，把其他状态设为不显示
-              this.queryParams.pageNum+=1;
-			  this.list = [...this.list, ...res.data];
-            } else { // 请求的数据没有4条，说明下一页已经暂时没有数据了，列表添加上新的数据，底部显示暂无更多数据
-              this.queryParams.pageNum+=1;
-			  this.list = [...this.list, ...res.data];
-              this.hasMoreData = false; // 设置为没有更多数据
-            }
-          }
-		  this.isLoading = false; // 设置为加载完成
-        })
-        .catch((error) => {
-          console.error("路由数据加载失败:", error);
-		  this.isLoading = false; // 设置为加载完成
-        });
-    },
-    handleClick(route) {
-      if (route.isAC === 1) {
-        // 提示用户已经作答
-        toast("该路线已作答！");
-        return;
-      }
-      // 跳转到填写问卷页面
-      const routeStr = JSON.stringify(route);
-      uni.navigateTo({
-        url: `/pages/componentsA/questionnaire/questionnaire_filling?route=${encodeURIComponent(
+	export default {
+		name: "route-choice",
+		data() {
+			return {
+				list: [], // 原始路线列表
+				isLoading: false, // 是否正在加载
+				queryParams: {
+					pageNum: 1,
+					pageSize: 3
+				},
+				 query:{
+					pageNum: 400,
+					pageSize: 1
+				},
+				endData:{
+					routerId:2
+				},
+				hasMoreData: true // 是否还有更多数据
+			};
+		},
+		created() {
+			// 页面创建时加载数据
+			this.fetchRoutes();
+			wx.showShareMenu({
+				withShareTicket: true,
+				menus: ['shareAppMessage', 'shareTimeline']
+			});
+		},
+		methods: {
+			// 分享好友
+			onShareAppMessage() {
+				return {
+					title: '小枣营造社骑行小工具',
+					path: `pages/index`
+				}
+			},
+			// 分享朋友圈
+			onShareTimeline() {
+				return {
+					title: '小枣营造社骑行小工具',
+					path: `pages/index`
+				}
+			},
+			fetchRoutes() {
+				this.isLoading = true; // 设置为正在加载
+				if (this.endData.routerId==2){
+					getRouters(this.query)
+						.then((res) => {
+							this.endData =res.data[0];
+						});
+				};
+				getRouters(this.queryParams)
+					.then((res) => {
+						if (res.data.length != 0) { // 请求的数据为空，没有数据
+							if (res.data.length < this.queryParams.pageSize) { // 请求的数据为4条，说明下一页可能还有数据，列表添加上新的数据，把其他状态设为不显示
+								this.list = [...this.list, ...res.data];
+								this.hasMoreData = false; // 设置为没有更多数据
+							} else { // 请求的数据没有4条，说明下一页已经暂时没有数据了，列表添加上新的数据，底部显示暂无更多数据
+								this.queryParams.pageNum += 1;
+								this.list = [...this.list, ...res.data];
+								if(res.data[this.queryParams.pageSize-1].routerId == this.endData.routerId){
+									this.hasMoreData = false; // 设置为没有更多数据
+								}
+							}
+						} else {
+																console.log("我被执行了22");
+							this.hasMoreData = false; // 设置为没有更多数据
+						}
+						this.isLoading = false; // 设置为加载完成
+					})
+					.catch((error) => {
+						console.error("路由数据加载失败:", error);
+						this.isLoading = false; // 设置为加载完成
+					});
+			
+			
+			},
+			handleClick(route) {
+				if (route.isAC === 1) {
+					// 提示用户已经作答
+					toast("该路线已作答！");
+					return;
+				}
+				// 跳转到填写问卷页面
+				const routeStr = JSON.stringify(route);
+				uni.navigateTo({
+					url: `/pages/componentsA/questionnaire/questionnaire_filling?route=${encodeURIComponent(
           routeStr
         )}`,
-      });
-    },
-  },
-};
+				});
+			},
+		},
+	};
 </script>
 
 
@@ -252,26 +269,39 @@ export default {
 		background-color: #1A98DA;
 		/* 蓝色标识单选题 */
 	}
+
 	/* styles.wxss */
 	.load-more-button {
-	  display: block;
-	  height: 50px;
-	  width: 60%; /* 按钮宽度 */
-	  margin: 20px auto; /* 水平居中，垂直外边距 */
-	  padding: 5px; /* 内边距 */
-	  background: linear-gradient(90deg, #00c6ff, #0072ff);
-	  color: white; /* 字体颜色 */
-	  border: none; /* 无边框 */
-	  border-radius: 30px; /* 圆角 */
-	  text-align: center; /* 文本居中 */
-	  font-size: 16px; /* 字体大小 */
+		display: block;
+		height: 50px;
+		width: 60%;
+		/* 按钮宽度 */
+		margin: 20px auto;
+		/* 水平居中，垂直外边距 */
+		padding: 5px;
+		/* 内边距 */
+		background: linear-gradient(90deg, #00c6ff, #0072ff);
+		color: white;
+		/* 字体颜色 */
+		border: none;
+		/* 无边框 */
+		border-radius: 30px;
+		/* 圆角 */
+		text-align: center;
+		/* 文本居中 */
+		font-size: 16px;
+		/* 字体大小 */
 	}
+
 	.load-more-button:hover {
-	  background-color: #179b16; /* 悬停时背景颜色 */
+		background-color: #179b16;
+		/* 悬停时背景颜色 */
 	}
-	
+
 	.load-more-button:disabled {
-	  background-color: #cccccc; /* 禁用时背景颜色 */
-	  cursor: not-allowed; /* 禁用时鼠标样式 */
+		background-color: #cccccc;
+		/* 禁用时背景颜色 */
+		cursor: not-allowed;
+		/* 禁用时鼠标样式 */
 	}
 </style>
